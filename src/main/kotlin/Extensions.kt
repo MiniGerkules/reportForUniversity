fd3b1cdd11f4f4b9ea8445bd79818ff69bd8c93b
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-10-09 09:43:11 +0300

    Added a new function. The function implements reverse indexing

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,6 +1,22 @@
-fun String.countChar(symbol: Char) : Int {
+fun String.countChar(symbol: Char): Int {
     var numberOfSymbol = 0
     this.forEach { curSym -> if (curSym == symbol) ++numberOfSymbol }
-
     return numberOfSymbol
+}
+
+fun String.revInd(index: Int): Char {
+    if ((index + this.length - 1 < 0) || (index > 0))
+        throw IllegalArgumentException("Out of bounds")
+
+    return this[index + this.length - 1]
+}
+
+fun String.splitToPair(): MutableList<String> {
+        val toReturn: MutableList<String> = mutableListOf()
+        val fullString: String = this + "_"
+
+        for (i: Int in this.indices step 2)
+            toReturn.add(fullString[i].toString() + fullString[i + 1])
+
+        return toReturn
 }
