e49d5ed94fea6f9eb547f0009812c5d99042b80a
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2021-10-10 13:09:44 +0300

    Updated class under test

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,35 +1,43 @@
 /**
  * A class that implements some arithmetic operations
- * @param first the first number to calculate
- * @param second the second number to calculate
  * @author Katser Eugene
  */
-class Calculator(val first: Int, val second: Int) {
-    /**
-     * The method performs the addition operation
-     * @return the total of the <code>first</code> and <code>second</code>
-     * @author Katser Eugene
-     */
-    fun add() = first + second
+class Calculator {
+    companion object {
+        /**
+         * The method performs the addition operation
+         * @return the total of the <code>first</code> and <code>second</code>
+         * @param first the first number to calculate
+         * @param second the second number to calculate
+         * @author Katser Eugene
+         */
+        fun add(first: Int, second: Int) = first + second
 
-    /**
-     * The method performs the addition operation
-     * @return the difference of the <code>first</code> and <code>second</code>
-     * @author Kater Eugene
-     */
-    fun subtract() = first - second
+        /**
+         * The method performs the addition operation
+         * @return the difference of the <code>first</code> and <code>second</code>
+         * @param first the first number to calculate
+         * @param second the second number to calculate
+         * @author Kater Eugene
+         */
+        fun subtract(first: Int, second: Int) = first - second
 
-    /**
-     * The method performs the multiply operation
-     * @return the product of the <code>first</code> and <code>second</code>
-     * @author Katser Eugene
-     */
-    fun multiply() = first * second
+        /**
+         * The method performs the multiply operation
+         * @return the product of the <code>first</code> and <code>second</code>
+         * @param first the first number to calculate
+         * @param second the second number to calculate
+         * @author Katser Eugene
+         */
+        fun multiply(first: Int, second: Int) = first * second
 
-    /**
-     * The method performs the addition operation
-     * @return the quotient of the <code>first</code> and <code>second</code>
-     * @author Katser Eugene
-     */
-    fun divide() = first / second
+        /**
+         * The method performs the addition operation
+         * @return the quotient of the <code>first</code> and <code>second</code>
+         * @param first the first number to calculate
+         * @param second the second number to calculate
+         * @author Katser Eugene
+         */
+        fun divide(first: Int, second: Int) = first / second
+    }
 }
